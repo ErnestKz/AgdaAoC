da80e87b9229875e5fd83edf99ce9d2306df1857
ernestkz <ernestkuznecovs@gmail.com>
2021-12-01 16:20:20 +0000

    Make pure.

--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
-with import <nixpkgs> {};
+with import (import ./nix/sources.nix).nixpkgs-unstable {};
 let
   agdaWithPackages = (agda.withPackages (ps: [
     ps.standard-library
     ps.agda-prelude
-
   ]));
 in
 mkShell {
